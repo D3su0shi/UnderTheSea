f0028f99036cf1854f8a8410be947dba9b3641af
hailia sommerville <196206726+haicomet@users.noreply.github.com>
2025-11-28 17:48:21 -0500

    feat: Create TimerSystem for centralized event management (oxygen bubble script created as well)
    - Created the central manager for all game timers.
    - Logic: Timers are added to a queue and sorted by finish time.
    - Added AddTimer, CancelTimer, and GetProgress methods.
    - Oxygen Bubble class created ( next to do)
    - Note: WeaponSystem will need to be updated to use TimerSystem.Instance instead of local float timers.

---
+++ Assets/script/OxygenBubble.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class OxygenBubble : MonoBehaviour
+{
+    private float dockingZoneRadius; //circular radius the ship must enter to collect oxygen
+    private float refillTimeDuration; //time taken to refill oxygen
+    private bool isRefilling = false; //flag to check if refilling is in progress
+    //public GameUI gameUI; //reference to game UI
+    public subScript ship; //reference to the ship
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
